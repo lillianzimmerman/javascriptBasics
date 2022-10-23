ff76070f0838cf27ab6092a8050f842a11b15536
Lily Zimmerman <lbzimm@outlook.com>
2022-10-23 16:58:11 -0400

    Undefined and Null file added

---
+++ 02-undefinedAndNull.js
@@ -0,0 +1,12 @@
+// UNDEFINED
+let coffeeCupContents;
+
+console.log(coffeeCupContents); // prints undefined
+
+// NULL 
+
+let userData = "User Name and Email Here";
+
+// Setting a variable to null to purposely remove its contents. 
+userDate = null;
+
