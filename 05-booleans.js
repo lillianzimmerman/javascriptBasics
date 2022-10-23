9545407fe50923c0a91c2ca1595969eaf1ad61a9
Lily Zimmerman <lbzimm@outlook.com>
2022-10-23 18:04:24 -0400

    Booleans notes added

---
+++ 05-booleans.js
@@ -0,0 +1,29 @@
+/* 
+YES / NO 
+ON / OFF 
+TRUE / FALSE 
+*/
+
+let isLoggedIn = true;
+let isAuthenticated = true;
+let hasLoggedInToday = false;
+let hasToken = false;
+
+console.log(2 == '2') // true
+console.log(2 === '2') // false (remember this compares type as well as value!)
+console.log(2 != 3) // true
+console.log(2 !== '2') // true (true since this operator compares type as well)
+console.log(5 > 5) // false
+console.log(5 >= 5) // true
+console.log(7 < 8) //true
+console.log(8 <= 8) //true 
+
+console.log(2 > 1); // true
+console.log(3 < 2); // false
+
+let test = 2 >= 3;
+console.log(test); // false
+
+console.log("Password12!" === "Password12!!");
+console.log(1 !== 2);
+console.log(10 !== 10);    
