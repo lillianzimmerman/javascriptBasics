64a928db68c34edd78ba96e7a6ee92f2036c05a8
Lily Zimmerman <lbzimm@outlook.com>
2022-10-23 17:19:48 -0400

    Numbers notes added

---
+++ 03-numbers.js
@@ -0,0 +1,33 @@
+// Numbers in JavaScript
+0
+1
+0.99
+-42
+NaN
+
+// Storing numbers in Variables
+let theMeaningOfLife = 42;
+const indianaIncomeTaxRate = 0.0323;
+let powerLevel = 9001;
+let southPoleTempF = -44;
+let spaceStationCrewCount = 7;
+const imNotANumber = NaN;
+
+
+// Arithmetic Operators 
+1 + 1; // Addition
+12 - 10; // Subtraction
+125 * 1.07; // Multiplication
+125 / 4; // Division
+10 ** 2; // Exponentiation
+15 % 2 // Modulus
+
+let billAmount = 132.96;
+let totalPeople = 5;
+const salesTax = 0.07;
+let taxAmount = billAmount * salesTax;
+let grandTotal = billAmount + taxAmount;
+let individualContribution = grandTotal / totalPeople;
+console.log(individualContribution);
+
+
