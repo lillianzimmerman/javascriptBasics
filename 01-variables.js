75d8d0ad7ebe026c850bf148497948d70176271f
Lily Zimmerman <lbzimm@outlook.com>
2022-10-23 16:47:51 -0400

    Intial Commit

---
+++ 01-variables.js
@@ -0,0 +1,23 @@
+// JavaScript Variable Declaration
+var myFirstVariable;
+
+// Initialization
+myFirstVariable = "hi";
+
+// Re-Initialization
+myFirstVariable = "Hello";
+
+// Declaration and Initialization
+var orangeJuicecontainer = "empty";
+
+// Var and Hoisting
+programmer = "Ada Lovelace";
+var programmer;
+
+// Let and Hoisting
+jsVersion = "es6";
+let jsVersion;
+
+// Const and Re-Initialization
+const humansBestFriend = "dog";
+humansBestFriend = "frog";
