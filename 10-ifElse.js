e34c0a799055fcd10c9097bea87cd7700e5b7970
Lily Zimmerman <lbzimm@outlook.com>
2022-10-23 18:27:11 -0400

    If Else notes added

---
+++ 10-ifElse.js
@@ -0,0 +1,30 @@
+//1 
+if (condition) {
+    // code to be executed if the condition is true
+  //2
+  } else { //3
+    // code to be executed if the condition is false
+    } 
+
+//Example 1
+if (10 > 5) {
+    console.log("10 is greater than 5")
+  } else {
+    console.log("10 is not greater than 5")
+  }
+  
+  //Example 2
+  if (10 < 5) {
+    console.log("10 is less than 5")
+  } else {
+    console.log("10 is not less than 5")
+  }
+  
+  //Example 3
+  let weather = "rainy"
+  
+  if (weather === "rainy"){
+    console.log("Bring an umbrella")
+  } else {
+    console.log("You don't need an umbrella")
+  }
