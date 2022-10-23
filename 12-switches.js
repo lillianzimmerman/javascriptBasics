fb8a4db733e6dd73ef220d61881ef953d8e21a15
Lily Zimmerman <lbzimm@outlook.com>
2022-10-23 18:38:55 -0400

    Switches notes added

---
+++ 12-switches.js
@@ -0,0 +1,27 @@
+switch(expression) {
+    case x: 
+        // code block 
+        break;
+    case y: 
+        // code block
+        break;
+    default:
+        // code block
+}
+
+let ingredients = "Salmon"
+
+switch (ingredients) {
+    case "Ground Beef":
+        console.log("I'll make hamburgers.")
+        break;
+    case "Salmon":
+        console.log("I'll make grilled salmon.")
+        break;
+    case "Chicken": 
+        console.log("I'll make chicken tacos.")
+        break;
+    default: 
+        console.log("I guess I'll order something.")
+}
+
