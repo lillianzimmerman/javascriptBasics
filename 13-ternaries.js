80f97f011c3b8edac159f1c6ade8432971c33717
Lily Zimmerman <lbzimm@outlook.com>
2022-10-23 18:46:29 -0400

    Ternaries notes added

---
+++ 13-ternaries.js
@@ -0,0 +1,5 @@
+// 1          // 2            // 3 
+// ( condition ) ? run this code : run this code instead
+
+let cookies = 5;
+(cookies > 0) ? console.log("I'm going to eat a cookie!") : console.log("I have no cookies :(") 
