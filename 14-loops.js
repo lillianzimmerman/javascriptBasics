ae4c84fa2cca5353b2d0ffdc3a27a8be9b74f669
Lily Zimmerman <lbzimm@outlook.com>
2022-10-25 18:07:11 -0400

    Loops notes added

---
+++ 14-loops.js
@@ -0,0 +1,33 @@
+for ([initialExpression]; [conditionalExpression]; [incrementExpression]){
+    // For loop body (code to be executed each loop)
+}
+
+for (let i = 1; i <=100; i++){
+    console.log(i);
+}
+
+// i++ = incrementing by one
+
+for (let i = 0; i <= 50; i += 5){
+    console.log(i);
+}
+
+for (let i = 20; i >= 1; i--){
+    console.log(i);
+}
+
+for (let i = 3; i < 30; i += 5){
+    console.log(i);
+}
+
+for (let i = 25; i <= 200; i += 25){
+    console.log(i);
+}
+
+for (let i = 6; i <= 10; i += 2){
+    console.log(i);
+}
+
+for (let i = 100; i >= 10; i-= 5){
+    console.log(i);
+}
