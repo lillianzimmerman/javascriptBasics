76fc651f3ba3491bc34f7eddeec164cf82c788f3
Lily Zimmerman <lbzimm@outlook.com>
2022-10-25 17:48:38 -0400

    Arrays notes added

---
+++ 08-arrays.js
@@ -0,0 +1,20 @@
+let rainbowColors = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet'];
+let raceWinners = [33, 72, 64];
+let myFavoriteThings = ['Broccoli', 46074,'Zombie Cats From Mars'];
+
+// Index of an array starts at 0 
+
+var countryArray = ["USA", "Japan", "Belgium"];
+
+console.log(countryArray[2]); // Belgium
+console.log(countryArray[0]); // USA 
+
+let dogs = ["Shiba Inu", "Husky", "Corgi", "Great Dane"];
+let cats = ["Maine Coon", "Bengal", "Siamese", "Sphynx"];
+let birds = ["African Grey Parrot", "Canary", "Macaw", "Finch"];
+let fish = ["Goldfish", "Neon Tetra", "Betta", "Guppy"];
+
+console.log(dogs[0]); // Shiba Inu
+console.log(cats[1]); // Bengal
+console.log(fish[3]); // Guppy
+console.log(birds[0]); // African Grey Parrot
